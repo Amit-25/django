b5320701d73502ceede0170d0c267dc40e963d75
Amit-25 <singhamit25may@gmail.com>
2023-05-13 12:33:00 +0530

    Tag application added

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store1.models import Product
 
 def hello(request):
+    query_set = Product.objects.select_related('collection')[:2]
+    print(query_set.query)
     context = {
+
         "error_code" : "200",
         "error_message" : "Success",
-        "msg": "Hello World!!"
+        "msg": "Hello World!!",
+        "product":list(query_set)
     }
-    return render(request, 'hello.html', context)
+    # return render(request, 'hello.html', context)
