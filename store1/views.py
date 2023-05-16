d2202db589af203ebccb1a29cfa43c6c15eefbe3
Amit-25 <singhamit25may@gmail.com>
2023-05-16 20:44:31 +0530

    Create Product List API using DRF

--- store1/views.py
+++ store1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view()
+def product_list(request):
+    context1 = {
+        "error_code": 1,
+        "error_message": "Success"
+    }
+    return Response(request)
+    # return Response('Ok')
