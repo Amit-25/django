375990bb84d0608ca5b204ff6f44948cbe573e47
Amit-25 <singhamit25may@gmail.com>
2023-05-17 10:17:31 +0530

    Add Collection/Product Serializer

--- store1/views.py
+++ store1/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product, Collection
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view()
 def product_list(request):
-    context1 = {
-        "error_code": 1,
-        "error_message": "Success"
+    query_set = Product.objects.select_related('collection').all()[:4]
+    serializer = ProductSerializer(query_set, many=True)
+    context = {
+        "data": serializer.data,
+        "error_code": 1
     }
-    return Response(request)
-    # return Response('Ok')
+    return Response(context)
+
+
+@api_view()
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
